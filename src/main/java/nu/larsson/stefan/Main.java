d97792a2cb400ab8f7b46484e51cfa7fb920345a
Stefan Larsson <313574+stela@users.noreply.github.com>
2025-12-29 20:17:02 +0100

    Initial empty project

---
+++ src/main/java/nu/larsson/stefan/Main.java
@@ -0,0 +1,13 @@
+package nu.larsson.stefan;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        IO.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
